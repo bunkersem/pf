50b65a366d7857d6efedfbc9181073390176e8dd
Sem <polyismstudio@gmail.com>
2017-08-26 12:28:19 +0200

    third commit

--- client/scripts/style.ts
+++ client/scripts/style.ts
@@ -1,33 +1,38 @@
 window.addEventListener('load', () => {
-    (function _iffyNavbar () {
-    // DO navbar stuff.
+    (function _iffyNavbar ():void {
+    // do navbar stuff.
     const nav: HTMLElement | null = document.getElementById('nav');
     var show: boolean = (nav && nav.classList.contains('show')) || false;
-    // Use SetTimeout to not relief the UI/Browser rendering.
+    var lockscroll = 0;
+    var lscroll = 0;
+    // use SetTimeout to not relief the UI/Browser rendering.
     window.addEventListener('scroll', (event) => setTimeout(function() {
-        if (show && window.scrollY > 600 && nav !== null) {
+        if (!show && (window.scrollY < 600 || window.scrollY < lscroll) && nav !== null) {
+            nav.classList.add('show');
+            show = true;
+            lockscroll = window.scrollY + 5;
+        }
+        else if (show && window.scrollY > 600 && window.scrollY > lscroll && nav !== null) {
             nav.classList.remove('show');
             show = false;
         }
-        else if (!show && window.scrollY < 600 && nav !== null) {
-            nav.classList.add('show');
-            show = true;
-        }
+    
+        lscroll = window.scrollY;
     }, 0))
 })();
 
-(function _iffyWrapper () {
-    // DO Page stuff.
-    const contentwrappers = document.getElementsByClassName('contentwrapper');
+(function _iffyWrapper (): void {
+    // do Page stuff.
+    const contentwrappers:HTMLCollectionOf<Element> = document.getElementsByClassName('contentwrapper');
     if (contentwrappers.length < 1){
         console.log('cannot find contentwrapper');
         return;
     }
-    // Use SetTimeout to not relief the UI/Browser rendering.
+    // use SetTimeout to not relief the UI/Browser rendering.
     window.addEventListener('resize', (event) => setTimeout(function() {
         for (var i = 0; i < contentwrappers.length; i++) {
             (<any>contentwrappers[i]).style.width = window.innerWidth + 'px';
         }
-    }, 0))
+    }, 0));
 })();
 }, false);
